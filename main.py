b675e34055c79b218dba57238ad516daeedda8a9
SudraviiDmitro <sudravijdmitro@gmail.com>
2026-02-28 13:38:33 +0200

    bleh

--- main.py
+++ main.py
@@ -10,30 +10,79 @@ fps = pygame.time.Clock()
 keys = pygame.key.get_pressed()
 
 class Player:
-    def __init__(self, x, y, width, heigt, images):
-        self.images = [pygame.transform.scale(pygame.image.load(img), (width, heigt)) for img in images]
+    def __init__(self, x, y, width, heigt, image, images_left, images_right):
+        self.images_left = [pygame.transform.scale(pygame.image.load(img), (width, heigt)) for img in images_left]
+        self.images_right = [pygame.transform.scale(pygame.image.load(img), (width, heigt)) for img in images_right]
+        self.image = pygame.transform.scale(pygame.image.load(image), (width, heigt))
         self.walk_speed = 2
-        self.ani_speed = 0.2
+        self.ani_speed = 0.09
         self.index = 0
-        self.hitbox = self.images[0].get_rect(topleft=(x,y))
-        self.current_image = self.images[0]
+        self.hitbox = self.image.get_rect(topleft=(x,y))
+        self.current_image = self.image
         
 
     def show(self):
         window.blit(self.current_image, (self.hitbox.x, self.hitbox.y))
 
-    def animate(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.current_image = pygame.image.load(self.images[self.cycle])
-            self.cycle = (self.cycle + 1) % len(self.images)
+    def animate_left(self):
+        if  event.type == pygame.KEYDOWN and keys[pygame.K_LEFT]:
+            self.index += self.ani_speed
             self.hitbox.x -= self.walk_speed
-            
-walking_left_img = []
-walking_right_img = []
-images =  ["StandingStill_test.png","Walking_test1.png","Walking_test2.png"]
-player = Player(50, 400, 60, 90, images)
+            if self.index >= len(self.images_left):
+                self.index = 0
+            self.current_image = self.images_left[int(self.index)]
 
+    def animate_right(self):
+        if  event.type == pygame.KEYDOWN and keys[pygame.K_RIGHT]:
+            self.index += self.ani_speed
+            self.hitbox.x += self.walk_speed
+            if self.index >= len(self.images_right):
+                self.index = 0
+            self.current_image = self.images_right[int(self.index)]
+
+    def stand(self):
+        if not event.type == pygame.KEYDOWN:
+            self.current_image = self.image
+
+
+class Door_l:
+    def __init__(self, x, y, width, heigt, image):
+        self.image = pygame.transform.scale(pygame.image.load(door_left), (width, heigt))
+        self.hitbox = self.image.get_rect(topleft=(x,y))
+        
+    def show(self):
+        window.blit(self.image, (self.hitbox.x, self.hitbox.y))
+
+
+class Door_r:
+    def __init__(self, x, y, width, heigt, image):
+        self.image = pygame.transform.scale(pygame.image.load(door_right), (width, heigt))
+        self.hitbox = self.image.get_rect(topleft=(x,y))
+        
+        
+    def show(self):
+        window.blit(self.image, (self.hitbox.x, self.hitbox.y))
+
+
+
+
+            
+
+images_left =  ["Walking_left1.png","Walking_left2.png"]
+images_right = ["Walking_right1.png","Walking_right2.png"]
+image = "Standing.png"
+
+door_left = "Door_1.png"
+door_right = "Door_2.png"
+
+bg1 = pygame.image.load("room_1.png")
+bg2 = pygame.image.load("room_2.png")
+bg3 = pygame.image.load("room_3.png")
+current_bg = bg1
+player = Player(50, 400, 60, 90, image, images_left, images_right)
+
+door_1 = Door_l(300, 370, 80, 120, door_left)
+door_2 = Door_r(800, 370, 80, 120, door_right)
 game = True
 while game:
     for event in pygame.event.get():
@@ -45,10 +94,22 @@ while game:
 
 
 
+    if player.hitbox.colliderect(door_1.hitbox) and keys[pygame.K_e]:
+        current_bg = bg2
+
+    if player.hitbox.colliderect(door_2.hitbox) and keys[pygame.K_e]:
+        current_bg = bg3
+
+
+
     
-    window.fill(WHITE)
+    window.blit(current_bg, (100,110))
     player.show()
-    player.animate()
+    player.animate_left()
+    player.animate_right()
+    player.stand()
+    door_1.show()
+    door_2.show()
     pygame.display.flip()
     fps.tick(60)
 
